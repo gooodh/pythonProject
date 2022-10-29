1e94053881ab8f03c3637b77820312ddc072ae5b
Ser <s.b.nikuliny@gmail.com>
2022-10-29 17:35:43 +0700

    m-video (fix)

--- M_video.py
+++ M_video.py
@@ -186,53 +186,53 @@ def get_data():
 
     with open('3_prices.json', 'w', encoding='utf-8') as file:
         json.dump(response, file, indent=4, ensure_ascii=False)
-#
-#     items_prices = {}
-#
-#     material_prices = response.get('body').get('materialPrices')
-#
-#     for item in material_prices:
-#         item_id = item.get('price').get('productId')
-#         item_base_price = item.get('price').get('basePrice')
-#         item_sale_price = item.get('price').get('salePrice')
-#         item_bonus = item.get('bonusRubles').get('total')
-#
-#         items_prices[item_id] = {
-#             'item_basePrice': item_base_price,
-#             'item_salePrice': item_sale_price,
-#             'item_bonus': item_bonus
-#         }
-#
-#     with open('4_items_prices.json', 'w', encoding='utf-8') as file:
-#         json.dump(items_prices, file, indent=4, ensure_ascii=False)
-#
-#
-# def get_result():
-#     with open('2_items.json', encoding='utf-8') as file:
-#         products_data = json.load(file)
-#
-#     with open('4_items_prices.json', encoding='utf-8') as file:
-#         products_prices = json.load(file)
-#
-#     products_data = products_data.get('body').get('products')
-#
-#     for item in products_data:
-#         product_id = item.get('productId')
-#
-#         if product_id in products_prices:
-#             prices = products_prices[product_id]
-#
-#         item['item_basePrice'] = prices.get('item_basePrice')
-#         item['item_salePrice'] = prices.get('item_salePrice')
-#         item['item_bonus'] = prices.get('item_bonus')
-#
-#     with open('5_result.json', 'w', encoding='utf-8') as file:
-#         json.dump(products_data, file, indent=4, ensure_ascii=False)
+
+    items_prices = {}
+
+    material_prices = response.get('body').get('materialPrices')
+
+    for item in material_prices:
+        item_id = item.get('price').get('productId')
+        item_base_price = item.get('price').get('basePrice')
+        item_sale_price = item.get('price').get('salePrice')
+        item_bonus = item.get('bonusRubles').get('total')
+
+        items_prices[item_id] = {
+            'item_basePrice': item_base_price,
+            'item_salePrice': item_sale_price,
+            'item_bonus': item_bonus
+        }
+
+    with open('4_items_prices.json', 'w', encoding='utf-8') as file:
+        json.dump(items_prices, file, indent=4, ensure_ascii=False)
+
+
+def get_result():
+    with open('2_items.json', encoding='utf-8') as file:
+        products_data = json.load(file)
+
+    with open('4_items_prices.json', encoding='utf-8') as file:
+        products_prices = json.load(file)
+
+    products_data = products_data.get('body').get('products')
+
+    for item in products_data:
+        product_id = item.get('productId')
+
+        if product_id in products_prices:
+            prices = products_prices[product_id]
+
+        item['item_basePrice'] = prices.get('item_basePrice')
+        item['item_salePrice'] = prices.get('item_salePrice')
+        item['item_bonus'] = prices.get('item_bonus')
+
+    with open('5_result.json', 'w', encoding='utf-8') as file:
+        json.dump(products_data, file, indent=4, ensure_ascii=False)
 
 
 def main():
     get_data()
-    # get_result()
+    get_result()
 
 
 if __name__ == '__main__':
