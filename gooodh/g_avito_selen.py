58e2c08934c2df9e9de0eb90ed5e2bfb49786ec1
Ser <s.b.nikuliny@gmail.com>
2022-12-08 21:40:19 +0700

    g_avito_selen.py(price)

--- gooodh/g_avito_selen.py
+++ gooodh/g_avito_selen.py
@@ -1,6 +1,7 @@
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 import time
 import json
 from selenium.webdriver.common.by import By
@@ -36,9 +37,28 @@ def get_source_html(url):
 def get_result():
     with open('page.html', 'r', encoding='utf-8') as file:
         products_data = file.read()
+    soup = BeautifulSoup(products_data, 'lxml')
+    soup_headers = soup.find_all('h3', class_='title-root-zZCwT body-title-drnL0 title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO')
+    soup_prices = soup.find_all('span', class_='price-text-_YGDY text-text-LurtD text-size-s-BxGpL')
+
+
+    filtere_price=[]
+
+    for price in soup_prices:
+        print(price.text)
+        # filtere_price.append(str(price.text))
+        # print(filtere_price)
+
+    # filtere_headers=[]
+
+    # for header in soup_headers:
+    #     filtere_price.append(header.text)
+    #     print(filtere_headers)
 
 
 
+# <h3 itemprop="name" class="title-root-zZCwT body-title-drnL0 title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO">4-к. квартира, 76&nbsp;м², 4/5&nbsp;эт.</h3>
+#<span class="price-text-_YGDY text-text-LurtD text-size-s-BxGpL">6&nbsp;500&nbsp;000<!-- -->&nbsp;<span class="price-currency-_FNLV">₽</span></span>
 if __name__ == '__main__':
     # url = 'https://www.avito.ru/barnaul/nedvizhimost'
     # get_source_html(url)
